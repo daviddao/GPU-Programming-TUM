43c26bc5d2387a1197d6cda0e1a80f8154d7fb01
Kennung Wintersemester Acc030 <w030@atcremers61.informatik.tu-muenchen.de>
2015-10-06 13:22:25 +0200

    some stuff in the end

---
+++ gpu_johannes/solve_LSE_CULA_float.cu
@@ -0,0 +1,65 @@
+/**
+ * David Dao, Johannes Rausch, Michal Szymczak
+ * TU Munich
+ * Sep 2015
+ */
+
+#include <stdio.h>
+#include "mex.h"
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <cuda_runtime.h>
+#include <cublas_v2.h>
+#include <cula.h>
+#include <cula_lapack.h>
+
+using namespace std;
+
+void checkStatus(culaStatus status)
+{
+char buf[80];
+
+if(!status)
+return;
+
+culaGetErrorInfoString(status, culaGetErrorInfo(), buf, sizeof(buf));
+printf("%s\n %d", buf, status);
+
+culaShutdown();
+//exit(EXIT_FAILURE);
+}
+
+/* Input arguments */
+#define IN_A		prhs[0]
+#define IN_B		prhs[1]
+
+/* Gateway routine */
+void mexFunction( int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[] )
+{
+  float *matrix_A, *matrix_B;
+  //float *d_matrix_A, *d_matrix_B;
+  int     N, D;
+  
+  /* Get the sizes of each input argument */
+  N = mxGetM(IN_A);
+  D = mxGetN(IN_B);
+  
+    /* Assign pointers to the input arguments */
+  matrix_A      = (float*) mxGetPr(IN_A);
+  matrix_B      = (float*) mxGetPr(IN_B);
+  
+  int matrix_pivot[N];
+  culaStatus status;
+  status = culaInitialize();
+  checkStatus(status);
+ 
+  status = culaSgesv(N, D, matrix_A, N, matrix_pivot, matrix_B, N);
+  checkStatus(status);
+  culaShutdown();
+  
+  return;
+}
+
+
+
